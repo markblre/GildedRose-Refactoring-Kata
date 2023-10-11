d9f28f99218aae432256134630da6dda8c15f66a
Mark Ballereau <mark.b@live.fr>
2023-10-12 01:40:35 +0200

    Création de la méthode updateQuality() dans la classe Item.

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -14,8 +14,59 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+
+    public void updateQuality() {
+      if (!this.name.equals("Aged Brie")
+        && !this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if (this.quality > 0) {
+          if (!this.name.equals("Sulfuras, Hand of Ragnaros")) {
+            this.quality = this.quality - 1;
+          }
+        }
+      } else {
+        if (this.quality < 50) {
+          this.quality = this.quality + 1;
+
+          if (this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (this.sellIn < 11) {
+              if (this.quality < 50) {
+                this.quality = this.quality + 1;
+              }
+            }
+
+            if (this.sellIn < 6) {
+              if (this.quality < 50) {
+                this.quality = this.quality + 1;
+              }
+            }
+          }
+        }
+      }
+
+      if (!this.name.equals("Sulfuras, Hand of Ragnaros")) {
+        this.sellIn = this.sellIn - 1;
+      }
+
+      if (this.sellIn < 0) {
+        if (!this.name.equals("Aged Brie")) {
+          if (!this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (this.quality > 0) {
+              if (!this.name.equals("Sulfuras, Hand of Ragnaros")) {
+                this.quality = this.quality - 1;
+              }
+            }
+          } else {
+            this.quality = this.quality - this.quality;
+          }
+        } else {
+          if (this.quality < 50) {
+            this.quality = this.quality + 1;
+          }
+        }
+      }
     }
 }
