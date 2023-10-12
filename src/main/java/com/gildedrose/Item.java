725bc20d89583f678991c56923fa5b658b475c6f
Mark Ballereau <mark.b@live.fr>
2023-10-13 00:37:28 +0200

    Refactoring du code pour les pass coulisses
    - Utilisation de Math.min() pour que la qualité ne soit jamais supérieur à 50.
    
    - Utilisation d'une expression ternaire pour incrémenter la qualité différemment selon la date d'expiration.

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -6,8 +6,8 @@ public class Item {
   public static final int MIN_QUALITY_PRODUCT = 0;
 
   public static final int EXPIRY_DATE = 0;
-  public static final int BACKSTAGE_PASSES_LIMIT_11 = 11;
-  public static final int BACKSTAGE_PASSES_LIMIT_6 = 6;
+  public static final int BACKSTAGE_PASSES_LIMIT_10 = 10;
+  public static final int BACKSTAGE_PASSES_LIMIT_5 = 5;
 
   public static final String AGED_BRIE = "Aged Brie";
   public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
@@ -51,27 +51,12 @@ public class Item {
 
         break;
       case BACKSTAGE_PASSES:
-        if (this.quality < MAX_QUALITY_PRODUCT) {
-          this.quality = this.quality + 1;
-
-          if (this.sellIn < BACKSTAGE_PASSES_LIMIT_11) {
-            if (this.quality < MAX_QUALITY_PRODUCT) {
-              this.quality = this.quality + 1;
-            }
-          }
-
-          if (this.sellIn < BACKSTAGE_PASSES_LIMIT_6) {
-            if (this.quality < MAX_QUALITY_PRODUCT) {
-              this.quality = this.quality + 1;
-            }
-          }
-        }
-
         this.sellIn = this.sellIn - 1;
 
-        if (this.sellIn < EXPIRY_DATE) {
-          this.quality = this.quality - this.quality;
-        }
+        this.quality =  (this.sellIn < EXPIRY_DATE) ? 0 :
+                        (this.sellIn < BACKSTAGE_PASSES_LIMIT_5) ? Math.min(this.quality + 3, MAX_QUALITY_PRODUCT) :
+                        (this.sellIn < BACKSTAGE_PASSES_LIMIT_10) ? Math.min(this.quality + 2, MAX_QUALITY_PRODUCT) :
+                        Math.min(this.quality + 1, MAX_QUALITY_PRODUCT);
 
         break;
       case SULFURAS:
